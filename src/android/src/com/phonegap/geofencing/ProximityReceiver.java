6a1140623125b8e86ef69ea6cb3efb7fb978bb31
Reinaldo Espana <respanaven@gmail.com>
2016-09-13 14:48:46 -0500

    Update ProximityReceiver.java

--- src/android/src/com/phonegap/geofencing/ProximityReceiver.java
+++ src/android/src/com/phonegap/geofencing/ProximityReceiver.java
@@ -11,19 +11,22 @@ import static com.phonegap.geofencing.DGGeofencing.TAG;
 public class ProximityReceiver extends BroadcastReceiver {
 
   @Override
-    public void onReceive(Context context, Intent intent) {
-    String id = (String) intent.getExtras().get("id");
-    Log.d(TAG, "received proximity alert for region " + id);
+  public void onReceive(Context context, Intent intent) {
+  String id = (String) intent.getExtras().get("id");
+  Log.d(TAG, "received proximity alert for region " + id);
 
-  if(DGGeofencing.getInstance() != null){
-    DGGeofencing.getInstance().fireRegionChangedEvent(intent);
-  }
-  else {
+    if(DGGeofencing.getInstance() != null){
+        DGGeofencing.getInstance().fireRegionChangedEvent(intent);
+    }  
+    else {
 
         String status = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false) ? "entering" : "exiting";
 
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        int defaults = Notification.DEFAULT_ALL;
+    intent=new Intent(context,CordovaApp.class);
+    intent.putExtra("pntype","1");
+
+    PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    int defaults = Notification.DEFAULT_ALL;
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                     .setDefaults(defaults)
@@ -34,7 +37,7 @@ public class ProximityReceiver extends BroadcastReceiver {
                     .setContentIntent(contentIntent)
                     .setAutoCancel(true);
 
-            String message = "Status of my event - "+status;
+            String message = "You are "+status+" to region.";
             if (message != null) {
                 mBuilder.setContentText(message);
             } 
@@ -51,7 +54,5 @@ public class ProximityReceiver extends BroadcastReceiver {
 
             mNotificationManager.notify((String) appName, notId, mBuilder.build());
         } 
-    
-}
-
+    }
 }

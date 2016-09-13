2ed8e2a67a569eae5dc311791d9b5ebb780c7d40
Reinaldo Espana <respanaven@gmail.com>
2016-09-13 14:43:00 -0500

    Update ProximityReceiver.java

--- src/android/src/com/phonegap/geofencing/ProximityReceiver.java
+++ src/android/src/com/phonegap/geofencing/ProximityReceiver.java
@@ -14,7 +14,44 @@ public class ProximityReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
     String id = (String) intent.getExtras().get("id");
     Log.d(TAG, "received proximity alert for region " + id);
+
+  if(DGGeofencing.getInstance() != null){
     DGGeofencing.getInstance().fireRegionChangedEvent(intent);
+  }
+  else {
+
+        String status = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false) ? "entering" : "exiting";
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        int defaults = Notification.DEFAULT_ALL;
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                    .setDefaults(defaults)
+                    .setSmallIcon(context.getApplicationInfo().icon)
+                    .setWhen(System.currentTimeMillis())
+                    .setContentTitle("Floral")
+                    .setTicker("Floral")
+                    .setContentIntent(contentIntent)
+                    .setAutoCancel(true);
+
+            String message = "Status of my event - "+status;
+            if (message != null) {
+                mBuilder.setContentText(message);
+            } 
+
+            String msgcnt = "1";
+            if (msgcnt != null) {
+                mBuilder.setNumber(Integer.parseInt(msgcnt));
+            }
+
+            int notId = 0;
+
+            NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            String appName = "Test";
+
+            mNotificationManager.notify((String) appName, notId, mBuilder.build());
+        } 
+    
 }
 
 }

006007c0497fd1a30ce067832d18e19b89c2c842
Reinaldo Espana <respanaven@gmail.com>
2016-09-15 12:45:46 -0500

    Update ProximityReceiver.java

--- src/android/src/com/phonegap/geofencing/ProximityReceiver.java
+++ src/android/src/com/phonegap/geofencing/ProximityReceiver.java
@@ -15,14 +15,11 @@ public class ProximityReceiver extends BroadcastReceiver {
   String id = (String) intent.getExtras().get("id");
   Log.d(TAG, "received proximity alert for region " + id);
 
-if(DGGeofencing.getInstance() != null){
-DGGeofencing.getInstance().fireRegionChangedEvent(intent);
-}
-
-else {
-
+    if(DGGeofencing.getInstance() != null){
+      DGGeofencing.getInstance().fireRegionChangedEvent(intent);
+    }
+   else {
         String status = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false) ? "entering" : "exiting";
-
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         int defaults = Notification.DEFAULT_ALL;
         NotificationCompat.Builder mBuilder =
@@ -34,24 +31,19 @@ else {
                     .setTicker("Floral")
                     .setContentIntent(contentIntent)
                     .setAutoCancel(true);
-
             String message = "Status of my event - "+status;
             if (message != null) {
                 mBuilder.setContentText(message);
             } 
-
             String msgcnt = "1";
             if (msgcnt != null) {
                 mBuilder.setNumber(Integer.parseInt(msgcnt));
             }
-
             int notId = 0;
-
             NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             String appName = "Test";
-
             mNotificationManager.notify((String) appName, notId, mBuilder.build());
-    }
+    } 
     
   }
   
